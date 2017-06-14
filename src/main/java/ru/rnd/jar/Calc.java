d56fea919c193050a10f2fbb2ae46b4fc3faa033
alex <alex>
2017-06-15 02:45:47 +0300

    +jar

---
+++ src/main/java/ru/rnd/jar/Calc.java
@@ -0,0 +1,12 @@
+package ru.rnd.jar;
+
+/**
+ * Created by alex on 15.06.17.
+ */
+public class Calc {
+
+    public static int justDoIt(int i, int j){
+        return i+j;
+    }
+
+}
---
+++ src/main/java/ru/rnd/jar/MainClass.java
@@ -0,0 +1,14 @@
+package ru.rnd.jar;
+
+import static ru.rnd.jar.Calc.justDoIt;
+
+/**
+ * Created by alex on 15.06.17.
+ */
+public class MainClass {
+
+
+    public static void main(String[] args) {
+        System.out.println(justDoIt(3,8));
+    }
+}
